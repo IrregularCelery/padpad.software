686f0b37995f71d6d8159c787e0f3461deb5a2f1
Mohsen Rad <irregular.celery@gmail.com>
2024-12-05 17:23:47 +0330

    Sandbox `Config-Manager`: Let `config` struct keeps its `file_path`

--- sandbox/config-manager/main.rs
+++ sandbox/config-manager/main.rs
@@ -1,13 +1,43 @@
 use dirs;
 use serde::{Deserialize, Serialize};
-use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use std::{error::Error, fs::File};
 use toml;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
+    #[serde(skip)]
+    file_path: String,
+
     database: DatabaseConfig,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            file_path: {
+                let config_file_name = "config-manager/config.toml";
+                let config_file_location = dirs::config_local_dir().unwrap();
+
+                format!(
+                    "{}/{}",
+                    if cfg!(debug_assertions) {
+                        "./target/config"
+                    } else {
+                        config_file_location.to_str().unwrap_or(".")
+                    },
+                    config_file_name
+                )
+            },
+            database: DatabaseConfig {
+                server: String::from("Server"),
+                ports: vec![80, 433],
+                connection_max: 32,
+                enabled: true,
+            },
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -18,8 +48,22 @@ struct DatabaseConfig {
     enabled: bool,
 }
 
-fn read_config(file_location: &str) -> Result<Config, Box<dyn std::error::Error>> {
+fn read_config() -> Result<Config, Box<dyn Error>> {
     let default_config = Config {
+        file_path: {
+            let config_file_name = "config-manager/config.toml";
+            let config_file_location = dirs::config_local_dir().unwrap();
+
+            format!(
+                "{}/{}",
+                if cfg!(debug_assertions) {
+                    "./target/config"
+                } else {
+                    config_file_location.to_str().unwrap_or(".")
+                },
+                config_file_name
+            )
+        },
         database: DatabaseConfig {
             server: String::from("Server"),
             ports: vec![80, 433],
@@ -28,12 +72,14 @@ fn read_config(file_location: &str) -> Result<Config, Box<dyn std::error::Error>
         },
     };
 
-    let path = Path::new(file_location);
+    let file_path = &default_config.file_path;
+
+    let path = Path::new(file_path);
     let mut file = match File::open(&path) {
         Ok(file) => file,
         Err(_) => {
             // If the file doesn't exist, create a new file
-            write_config(file_location, &default_config)?;
+            write_config(&default_config)?;
 
             File::open(&path)?
         }
@@ -43,12 +89,36 @@ fn read_config(file_location: &str) -> Result<Config, Box<dyn std::error::Error>
 
     file.read_to_string(&mut toml_str)?;
 
-    let config: Config = toml::from_str(&toml_str)?;
+    let config: Config = toml::from_str(&toml_str).unwrap_or_default();
 
     Ok(config)
 }
 
-fn write_config(file_location: &str, config: &Config) -> Result<File, Box<dyn std::error::Error>> {
+fn write_config(config: &Config) -> Result<File, Box<dyn Error>> {
+    let default_config = Config {
+        file_path: {
+            let config_file_name = "config-manager/config.toml";
+            let config_file_location = dirs::config_local_dir().unwrap();
+
+            format!(
+                "{}/{}",
+                if cfg!(debug_assertions) {
+                    "./target/config"
+                } else {
+                    config_file_location.to_str().unwrap_or(".")
+                },
+                config_file_name
+            )
+        },
+        database: DatabaseConfig {
+            server: String::from("Server"),
+            ports: vec![80, 433],
+            connection_max: 32,
+            enabled: true,
+        },
+    };
+
+    let file_location = &default_config.file_path;
     let path = Path::new(file_location);
     let parent_folder = path.parent().unwrap();
     std::fs::create_dir_all(parent_folder)?;
@@ -67,36 +137,26 @@ fn write_config(file_location: &str, config: &Config) -> Result<File, Box<dyn st
 }
 
 fn main() {
-    let config_file_name = "config-manager/config.toml";
-    let config_file_location = dirs::config_local_dir().unwrap();
+    loop {
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+        // Read configuration from file
+        let mut config = match read_config() {
+            Ok(config) => config,
+            Err(err) => {
+                eprintln!("Error reading config: {}", err);
 
-    let config_file_path = format!(
-        "{}/{}",
-        if cfg!(debug_assertions) {
-            "./target/config"
-        } else {
-            config_file_location.to_str().unwrap_or(".")
-        },
-        config_file_name
-    );
+                return;
+            }
+        };
 
-    // Read configuration from file
-    let mut config = match read_config(&config_file_path) {
-        Ok(config) => config,
-        Err(err) => {
-            eprintln!("Error reading config: {}", err);
+        println!("Config database: {:?}", config.database);
 
-            return;
+        // Modify config if needed
+        config.database.enabled = false;
+
+        // Write modified config back to file
+        if let Err(err) = write_config(&config) {
+            eprintln!("Error writing config: {}", err);
         }
-    };
-
-    println!("Config Server: {}", config.database.server);
-
-    // Modify config if needed
-    config.database.enabled = false;
-
-    // Write modified config back to file
-    if let Err(err) = write_config(&config_file_path, &config) {
-        eprintln!("Error writing config: {}", err);
     }
 }
