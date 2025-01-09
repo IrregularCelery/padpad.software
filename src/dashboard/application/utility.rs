6ca2c507dfe07e732804c8debb1bc85e76582b3a
Mohsen Rad <irregular.celery@gmail.com>
2025-01-09 18:40:33 +0330

    Move `request_send_serial()` function out of `Application` struct and to `application/utility.rs`

---
+++ src/dashboard/application/utility.rs
@@ -0,0 +1,7 @@
+use padpad_software::{constants::SERIAL_MESSAGE_SEP, tcp::client_to_server_message};
+
+pub fn request_send_serial(message: &str) -> Result<String, String> {
+    let request = format!("send_serial{}{}", SERIAL_MESSAGE_SEP, message);
+
+    client_to_server_message(&request)
+}
